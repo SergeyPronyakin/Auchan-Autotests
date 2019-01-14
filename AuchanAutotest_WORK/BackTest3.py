c961125845ecff71d3d52b3dbb19b6d691dacd84
SergeyPronyakin <36204226+SergeyPronyakin@users.noreply.github.com>
2019-01-14 16:44:20 +0300

    Edit

--- AuchanAutotest_WORK/BackTest3.py
+++ AuchanAutotest_WORK/BackTest3.py
@@ -6,8 +6,6 @@ from AuchanBack.AuchanRequests import *
 from app.application import *
 
 
-
-
 def main():
     auchanRequests.check_all_links_admin('https://preprod-auchan.atalan.net/pokupki/index.php/admin/dashboard/index/key/9f92511187ffde0ab55231d2364426c247a417cd5effb1c3950a5cd79de4e314/')
 
